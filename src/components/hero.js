694c06560fc06107f250b2c16b10e26d529f77e0
Adrien Thiery <adrien.thiery@osedea.com>
2020-05-11 15:39:25 +0200

    Finished programme page by adding address part

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,19 +6,21 @@ import PrefixedImage from "./prefixed-image"
 export default function Hero(props) {
     return (
         <article className={[style.container, props.dark && style.dark].join(' ')}>
-            <div className={['bordered', style.title].join(' ')}>
-                {props.title}
-            </div>
-            <div className={[style.media, style.image].join(' ')}>
+            {props.title && (
+                <div className={['bordered', style.title].join(' ')}>
+                    {props.title}
+                </div>
+            )}
+            <div className={[style.media, props.title && style.withtitle].join(' ')}>
                 <PrefixedImage src={props.media} cover />
                 {props.hasVideo && <PrefixedImage src='/assets/play.png' className={style.play} />}
             </div>
             <div className={['bordered', style.side, props.bigSide && style.big].join(' ')}>
+                {props.sideTitle && <header>{props.sideTitle}</header>}
                 {props.sideHtml
                     ? <p dangerouslySetInnerHTML={{ __html: props.sideHtml }} />
                     : <p>{props.side}</p>
                 }
-                {/* Logos? */}
             </div>
         </article>
     )
